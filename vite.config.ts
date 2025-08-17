cba7cced514c7a9834125cc45a5cde4ee0a85c06
NS <2300030644@kluniversity.in>
2025-08-17 12:00:40 +0530

    / added in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/text-to-essence/",   // 👈 VERY IMPORTANT for GitHub Pages
+  base: "/",   // 👈 VERY IMPORTANT for GitHub Pages
   server: {
     host: "::",
     port: 8080,
